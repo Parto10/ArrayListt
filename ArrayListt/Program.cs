30f00919eb81acb4c793d047a8e7b39d1a5cde55
ahad soory <soory2_2006@hotmail.com>
2016-10-04 09:38:06 +0330

    Initial Commit

---
+++ ArrayListt/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace ArrayListt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList Array = new ArrayList();
+            for (int intIndex = 0; intIndex <= 5; intIndex++)
+            {
+                Array.Add(intIndex);
+            }
+            Array.RemoveAt(0);
+            Array.Add(8);
+            foreach (int item in Array)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
